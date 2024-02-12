12b5d537efe21093493419f95d4a8ce99229ee15
King_Jireh <samuelsherpa46@gmail.com>
2024-02-12 09:27:03 +0545

    Complete the content of 06. Postman

--- 02. HTTP/06. Postman/MyFirstApp/MyFirstApp/Program.cs
+++ 02. HTTP/06. Postman/MyFirstApp/MyFirstApp/Program.cs
@@ -14,21 +14,10 @@ namespace MyFirstApp
             {
                 context.Response.Headers["content-type"] = "text/html";
 
-                if (context.Request.Headers.ContainsKey("user-agent"))
+                if (context.Request.Headers.ContainsKey("AuthorizationKey"))
                 {
-                    var userAgentValue = context.Request.Headers["user-agent"];
-                    await context.Response.WriteAsync($"<p>The value in key User-Agent is: {userAgentValue}</p>");
-                }
-
-                //below is my practice:
-                if ((context.Request.Headers.ContainsKey("accept")) && (context.Request.Headers.ContainsKey("accept-language")))
-                {
-                    var accept = context.Request.Headers["accept"];
-                    await context.Response.WriteAsync($"<p>The value in key Accept is: {accept}</p>");
-
-                    var acceptLanguage = context.Request.Headers["accept-language"];
-                    await context.Response.WriteAsync($"<p>The value in key Accept-Language is: {acceptLanguage}</p>");
-
+                    var authorizationKey = context.Request.Headers["AuthorizationKey"];
+                    await context.Response.WriteAsync($"<p>The value in AuthorizationKey is: {authorizationKey}</p>");
                 }
 
             });
