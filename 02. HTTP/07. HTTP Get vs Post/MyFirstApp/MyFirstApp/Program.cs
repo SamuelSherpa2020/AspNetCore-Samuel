af70171852c430a9e2e54a1b4aec18d9b83faad6
King_Jireh <samuelsherpa46@gmail.com>
2024-02-12 21:20:08 +0545

    Add project to 07. HTTP Get vs Post

---
+++ 02. HTTP/07. HTTP Get vs Post/MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MyFirstApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            var app = builder.Build();
+            app.Run(async (HttpContext context) =>
+            {
+                context.Response.Headers["content-type"] = "text/html";
+
+                if (context.Request.Headers.ContainsKey("AuthorizationKey"))
+                {
+                    var authorizationKey = context.Request.Headers["AuthorizationKey"];
+                    await context.Response.WriteAsync($"<p>The value in AuthorizationKey is: {authorizationKey}</p>");
+                }
+
+            });
+
+            app.Run();
+        }
+    }
+}
