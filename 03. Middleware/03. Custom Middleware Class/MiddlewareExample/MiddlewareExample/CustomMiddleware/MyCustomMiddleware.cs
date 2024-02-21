63c057a4708f88865ecec12cc3779b7285e50749
King_Jireh <samuelsherpa46@gmail.com>
2024-02-21 07:30:34 +0545

    Add complete code for 03. Custom Middleware Class

---
+++ 03. Middleware/03. Custom Middleware Class/MiddlewareExample/MiddlewareExample/CustomMiddleware/MyCustomMiddleware.cs
@@ -0,0 +1,12 @@
+﻿namespace MiddlewareExample.CustomMiddleware
+{
+    public class MyCustomMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("2nd Middleware Starts\n");
+            await next(context);
+            await context.Response.WriteAsync("2nd Middleware ends.\n");
+        }
+    }
+}
--- 03. Middleware/03. Custom Middleware Class/MiddlewareExample/MiddlewareExample/Program.cs
+++ 03. Middleware/03. Custom Middleware Class/MiddlewareExample/MiddlewareExample/Program.cs
@@ -1,3 +1,5 @@
+using MiddlewareExample.CustomMiddleware;
+
 namespace MiddlewareExample
 {
     public class Program
@@ -5,29 +7,31 @@ namespace MiddlewareExample
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddTransient<MyCustomMiddleware>();
+
             var app = builder.Build();
 
            
 
             //Middleware Chain 1
             app.Use(async(HttpContext context,RequestDelegate next)=>{
-                await context.Response.WriteAsync("1st Middleware\n");
+                await context.Response.WriteAsync("1st Middleware starts\n");
                 await next(context);
+                await context.Response.WriteAsync("1st Middleware Ends\n");
+
             });
 
-            //Middleware Chain 2
-            app.Use(async (HttpContext context, RequestDelegate next) => {
-                await context.Response.WriteAsync("2nd Middleware\n");
-                await next(context);
-            });
+            app.UseMiddleware<MyCustomMiddleware>();
 
             //Middleware Chain 2
             app.Use(async (HttpContext context, RequestDelegate next) => {
                 await context.Response.WriteAsync("3rd Middleware\n");
                 await next(context);
             });
+
             app.Run(async (HttpContext context) => {
-                await context.Response.WriteAsync("Terminating Middleware");
+                await context.Response.WriteAsync("Terminating Middleware\n");
             });
 
             app.Run();
