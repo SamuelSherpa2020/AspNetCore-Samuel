26377c98d07092996b31ee427babef0f6b6b970f
King_Jireh <samuelsherpa46@gmail.com>
2024-02-21 10:33:42 +0545

    Add delay code in CustomMiddleware code

--- 03. Middleware/03. Custom Middleware Class/MiddlewareExample/MiddlewareExample/CustomMiddleware/MyCustomMiddleware.cs
+++ 03. Middleware/03. Custom Middleware Class/MiddlewareExample/MiddlewareExample/CustomMiddleware/MyCustomMiddleware.cs
@@ -6,8 +6,13 @@ namespace MiddlewareExample.CustomMiddleware
     {
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
+            await Task.Delay(10000);
             await context.Response.WriteAsync("2nd Middleware Starts\n");
-            await next(context);
+
+            await Task.Delay(1000);
+            //return next(context);
+            await Task.Delay(1000);
+
             await context.Response.WriteAsync("2nd Middleware ends.\n");
         }
     }
