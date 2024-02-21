90a5b396b97920b51d6be87023d7d636360c954d
King_Jireh <samuelsherpa46@gmail.com>
2024-02-21 07:13:05 +0545

    Remove comment

--- 03. Middleware/03. Custom Middleware Class/MiddlewareExample/MiddlewareExample/Program.cs
+++ 03. Middleware/03. Custom Middleware Class/MiddlewareExample/MiddlewareExample/Program.cs
@@ -7,27 +7,7 @@ namespace MiddlewareExample
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            #region Middleware Chain
-            ////Middleware 1
-            //app.Use(async (HttpContext context,RequestDelegate next) =>
-            //{
-            //    await context.Response.WriteAsync("hello");
-            //    await next(context);
-            //});
-
-            ////Middleware 2
-            //app.Use(async (context, next) =>
-            //{
-            //    await context.Response.WriteAsync("Hello again");
-            //    await next(context);
-            //});
-
-            //app.Run(async (HttpContext context) =>
-            //{
-            //    await context.Response.WriteAsync("terminal/shortcicuit middleware is called");
-            //});
-
-            #endregion
+           
 
             //Middleware Chain 1
             app.Use(async(HttpContext context,RequestDelegate next)=>{
