b1e864141d1b384f3742129edbbb34a32a78aee4
King_Jireh <samuelsherpa46@gmail.com>
2024-02-26 08:02:28 +0545

    Add code for 02. Map, MapGet, MapPost

--- 04. Routing/02. Map, MagGet, MapPost/RoutingExample/RoutingExample/Program.cs
+++ 04. Routing/02. Map, MagGet, MapPost/RoutingExample/RoutingExample/Program.cs
@@ -12,7 +12,20 @@ namespace RoutingExample
 
             app.UseEndpoints(endpoints =>
             {
-                //add your endpoints
+                endpoints.MapGet("Map1",async (context) =>
+                {
+                   await context.Response.WriteAsync("URL Map1 been called");
+                });
+
+                endpoints.MapPost("Map2", async (context) =>
+                {
+                    await context.Response.WriteAsync("URL Map2 been found");
+                });
+            });
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync($"The program is running in {context.Request.Path} URL.");
             });
 
             app.Run();
