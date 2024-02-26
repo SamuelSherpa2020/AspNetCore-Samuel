fb76b982682fa49ebd25c2933c735387898cbb78
King_Jireh <samuelsherpa46@gmail.com>
2024-02-26 19:18:04 +0545

    Add code to add GetEndPoint and add UseEndpoints in the code

--- 04. Routing/Samuel Rocks/RoutingExample/RoutingExample/Program.cs
+++ 04. Routing/Samuel Rocks/RoutingExample/RoutingExample/Program.cs
@@ -7,12 +7,47 @@ namespace RoutingExample
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
+
             //enable routing
             app.UseRouting();
 
+
+            app.Use(async (context, next) =>
+            {
+                Endpoint? endPoint = context.GetEndpoint();
+                if(endPoint != null)
+                {
+                    await context.Response.WriteAsync($"Endpoint path: {endPoint.DisplayName} \n");
+                }
+                await next(context);
+            });
+
+            #region Use of UseEndpoints
+
+            // The below is also a way of 
+            //app.MapGet("/Map1", async (context) =>
+            //{
+            //    await context.Response.WriteAsync("I am first Map");
+            //});
+
             app.UseEndpoints(endpoints =>
             {
-                //add your endpoints
+                endpoints.MapGet("/Map1", async (context) =>
+                {
+                    await context.Response.WriteAsync($"Endpoint: {context.Request.Path} has been called.");
+                });
+
+                endpoints.MapPost("/Map2", async (context) =>
+                {
+                    await context.Response.WriteAsync($"Endpoint: {context.Request.Path} has been called.");
+                });
+            });
+
+            #endregion
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("I am terminating middleware");
             });
 
             app.Run();
