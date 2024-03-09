06a4a984e5a09b3acc46421e7bd9ff9d5d0ae9d0
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-03-09 21:18:04 +0545

    Add code for 01. Creating Controllers

--- 05. Controllers & IActionResult/01. Creating Controllers/ControllersExample/ControllersExample/Program.cs
+++ 05. Controllers & IActionResult/01. Creating Controllers/ControllersExample/ControllersExample/Program.cs
@@ -5,9 +5,20 @@ namespace ControllersExample
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            builder.Services.AddControllers();
 
-            app.MapGet("/", () => "Hello World!");
+            var app = builder.Build();
+           
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.Map("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World !");
+                });
+            });
+            app.MapControllers();
+            //app.MapGet("/", () => "Hello World!");
 
             app.Run();
         }
