a425475a16a6e64d7fdb026a6a83e56ebc810d72
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-03-18 19:27:37 +0545

    Add Code for 03. JsonResult

---
+++ 05. Controllers & IActionResult/03. JsonResult/ControllersExample/ControllersExample/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersExample.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        //public HomeController() { }
+        [Route("Home/Index")]
+        [Route("/")]
+        public ContentResult Index()
+        {
+            return Content("<h1>Hello there</h1></br><h2>I am calling you from Index</h2>","text/html");
+        }
+
+        [Route("Home/try")]
+        public string  Try()
+        {
+            return ("Hello from try");
+        }
+
+        [Route("Home/AboutUs")]
+        public string AboutUs()
+        {
+            string apple = HttpContext.Request.Path;
+            return apple;
+
+        }
+
+        [Route("Home/ContactUs/{mobile:regex(^\\d{{10}}$)}")]
+        public string Contact()
+        {
+            //var phoneNumber = HttpContext.Request.RouteValues["mobile"];
+
+             return("The given phone number is haha");
+        }
+        [Route("TryContentResult")]
+        public ContentResult TryContentResult()
+        {
+            return new ContentResult()
+            {
+                Content = "<h1>Hello there, I am from TryContentResult</h1>",
+                ContentType = "text/html"
+            };
+        }
+    }
+}
