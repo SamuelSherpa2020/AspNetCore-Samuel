6aae5ed4cecbeeff8928f9e8dffa3f89277cee02
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-03-25 09:36:31 +0545

    Addproject and completet the code for IActionResult

---
+++ 05. Controllers & IActionResult/05. IActionResult/IActionResultExample/IActionResultExample/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResultExample.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [Route("book")]
+        public IActionResult Index()
+        {
+            bool bookKey = Convert.ToBoolean(Request.Query.ContainsKey("bookid"));
+            var bookId = Convert.ToInt32(Request.Query["bookid"]);
+            bool isLogedIn = Convert.ToBoolean(Request.Query["isLoggedIn"]);
+
+            if (!bookKey)
+            {
+                Response.StatusCode = 400;
+                return Content("The bookid is not supplied");
+            }
+            if (!string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
+            {
+                Response.StatusCode = 400;
+                return Content("The bookid cannot be null or empty.");
+            }
+            if (bookId <= 0)
+            {
+                Response.StatusCode = 400;
+                return Content("The bookid cannot be less then or equal to zero.");
+            }
+            if (bookId > 1000)
+            {
+                Response.StatusCode = 400;
+                return Content("The bookid cannot be greater then 1000.");
+            }
+            return File("/simplefile1.pdf","application/pdf");
+        }
+    }
+}
