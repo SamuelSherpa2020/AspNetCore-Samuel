0c91a9dd061e41480b7bcb47009d13b0e202cadd
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-03-31 08:57:22 +0545

    Compelete code for 03. Model

--- 06. Model Binding and Validations/03. Model Class/IActionResultExample/IActionResultExample/Controllers/HomeController.cs
+++ 06. Model Binding and Validations/03. Model Class/IActionResultExample/IActionResultExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IActionResultExample.Model;
+using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 
 namespace IActionResultExample.Controllers
@@ -7,7 +8,7 @@ namespace IActionResultExample.Controllers
     {
 
         [Route("bookstore/{bookId?}/{isLoggedIn?}")]
-        public IActionResult Index([FromRoute]int?bookId,[FromQuery]bool?isLoggedIn)
+        public IActionResult Index([FromRoute]int?bookId,[FromQuery]bool?isLoggedIn,Book book)
         {
 
             #region Error Handling in isLoggedIn 
@@ -35,6 +36,8 @@ namespace IActionResultExample.Controllers
             }
             #endregion
 
+
+            #region Validation Of Request Data
             if (bookId.HasValue.Equals(false))
             {
                 return BadRequest("The bookid is not supplied");
@@ -55,13 +58,15 @@ namespace IActionResultExample.Controllers
             {
                 return Unauthorized("Use must be logged in");
             }
+            #endregion
             //return File("/simplefile1.pdf", "application/pdf");
 
             //return new RedirectToActionResult("Books", "Store", new { },permanent:true); // permanently telling browser to remember that the url has been changed permanently.
 
             //return RedirectToActionPermanent("Books","Store", new { Id=bookId});
 
-            return Redirect($"store/books/{bookId}");
+            //return Redirect($"store/books/{bookId}");
+            return Content($"BooId:{bookId} Book: {book}","text/plain");
         }
     }
 }
