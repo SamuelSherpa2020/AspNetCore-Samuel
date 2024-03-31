3776f1344f9c90837fb41ac2d4fb465b41741410
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-03-31 20:15:49 +0545

    Add project for model state

---
+++ 06. Model Binding and Validations/06. Model State/ModelValidatonExample/ModelValidatonExample/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidatonExample.Models;
+
+namespace ModelValidatonExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("register")]
+        public IActionResult Index(Person person)
+        {
+            return Content($"{person}");
+        }
+    }
+}
