cc8135322dd4df9ec2280e2cf6aa177dfe765d8c
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-03-31 19:58:40 +0545

    Add project and code in 05. Model Validations

---
+++ 06. Model Binding and Validations/05. Introduction to Model Validations/ModelValidatonExample/ModelValidatonExample/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidatonExample.Models
+{
+    public class Person
+    {
+        [Required]
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmedPassword { get; set; }
+        public string? Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person object - PersonName:{PersonName} Email: {Email} Phone: {Phone} Password: {Password} ConfirmePassword: {ConfirmedPassword} Price: {Price} ";
+        }
+    }
+}
