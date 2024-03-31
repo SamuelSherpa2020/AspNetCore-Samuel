244a80570add6d239c7dcf7471cd86679a34dfbb
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-03-31 20:58:43 +0545

    Add complete code for ModelState

--- 06. Model Binding and Validations/06. Model State/ModelValidatonExample/ModelValidatonExample/Controllers/HomeController.cs
+++ 06. Model Binding and Validations/06. Model State/ModelValidatonExample/ModelValidatonExample/Controllers/HomeController.cs
@@ -7,7 +7,37 @@ namespace ModelValidatonExample.Controllers
     {
         [Route("register")]
         public IActionResult Index(Person person)
-        {
+        {  
+
+            if (!ModelState.IsValid)
+            {
+                //List<string> errorMessages = new List<string>();
+                //foreach (var item in ModelState.Values)
+                //{
+                //    if(item.Errors.Count > 0)
+                //    {
+
+                //    }
+                //    foreach (var error in item.Errors)
+                //    {
+                //        errorMessages.Add(error.ErrorMessage);
+                //    }
+                //}
+                //Below is linq with which we made the above code short
+                
+                string errorList = string.Join(",", ModelState.Values
+                    .SelectMany(errors => errors.Errors)
+                    .Select(errorMessage => errorMessage.ErrorMessage).ToList());
+
+                string fullErrorMessage = string.Join(", ", ModelState.Values
+                    .Where(v => v.Errors.Count > 0) // using count of erors
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage));
+
+
+                return BadRequest(errorList);
+            }
+
             return Content($"{person}");
         }
     }
