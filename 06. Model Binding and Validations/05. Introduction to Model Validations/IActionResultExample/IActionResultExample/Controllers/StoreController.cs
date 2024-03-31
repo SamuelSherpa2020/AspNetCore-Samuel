3dce3f3f40b366b5ccf7dd8f48cfb0458dd10a8c
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-03-31 19:26:18 +0545

    Add code for model validation

---
+++ 06. Model Binding and Validations/05. Introduction to Model Validations/IActionResultExample/IActionResultExample/Controllers/StoreController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResultExample.Controllers
+{
+    public class StoreController : Controller
+    {
+
+        [Route("store/books/{id}")]
+        public IActionResult Books()
+        {
+            var id = Convert.ToInt32(Request.RouteValues["id"]);
+            return Content($"<h1>The id of the book is: {id}</h1>", "text/html");
+        }
+    }
+}
