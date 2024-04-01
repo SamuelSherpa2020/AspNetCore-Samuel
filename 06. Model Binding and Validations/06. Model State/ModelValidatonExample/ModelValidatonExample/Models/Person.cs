faf85d172192555187750a111a7865adbeebbaa5
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-04-01 10:35:12 +0545

    Add and complete project for Model Validation part 1

--- 06. Model Binding and Validations/06. Model State/ModelValidatonExample/ModelValidatonExample/Models/Person.cs
+++ 06. Model Binding and Validations/06. Model State/ModelValidatonExample/ModelValidatonExample/Models/Person.cs
@@ -5,12 +5,32 @@ namespace ModelValidatonExample.Models
 {
     public class Person
     {
-        [Required(ErrorMessage ="Person cannot be null 😅")]
+        //[Required(ErrorMessage ="Person cannot be null 😅")]
+        //public string? PersonName { get; set; }
+        //public string? Email { get; set; }
+        //public string? Phone { get; set; }
+        //public string? Password { get; set; }
+        //public string? ConfirmedPassword { get; set; }
+        //public string? Price { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return $"Person object - PersonName:{PersonName} Email: {Email} Phone: {Phone} Password: {Password} ConfirmePassword: {ConfirmedPassword} Price: {Price} ";
+        //}
+         //[Required(ErrorMessage = "{0} cannot be empty")]
+        //[Display(Name = "Person Name")]
+        //[StringLength(40, MinimumLength = 3, ErrorMessage = "{0} length should be between {2} and {1}")]
         public string? PersonName { get; set; }
+
+        [EmailAddress]
         public string? Email { get; set; }
+
         public string? Phone { get; set; }
+
         public string? Password { get; set; }
         public string? ConfirmedPassword { get; set; }
+
+        //[Range(0, 999.99, ErrorMessage = "{} must be between {1} and {2}")]
         public string? Price { get; set; }
 
         public override string ToString()
---
+++ 06. Model Binding and Validations/07. All Model Validations/ModelValidatonExample/ModelValidatonExample/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidatonExample.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "{0} cannot be empty")]
+        [Display(Name = "person name")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} length should be between {2} and {1}")]
+        public string? PersonName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Password { get; set; }
+        public string? ConfirmedPassword { get; set; }
+
+        [Range(0, 999.99, ErrorMessage = "{0} must be between {1} and {2}")]
+        public string? Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person object - PersonName:{PersonName} Email: {Email} Phone: {Phone} Password: {Password} ConfirmePassword: {ConfirmedPassword} Price: {Price} ";
+        }
+    }
+}
