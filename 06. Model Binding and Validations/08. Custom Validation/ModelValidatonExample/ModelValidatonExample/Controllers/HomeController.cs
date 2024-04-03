b580da877cd3da3ee088031e84108506cc0cdd96
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-04-03 18:27:46 +0545

    Add project in 08. Custom Validation

---
+++ 06. Model Binding and Validations/08. Custom Validation/ModelValidatonExample/ModelValidatonExample/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidatonExample.Models;
+
+namespace ModelValidatonExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("register")]
+        public IActionResult Index(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                List<string> errorMessages = new List<string>();
+                foreach (var item in ModelState.Values)
+                {
+                    if (item.Errors.Count > 0)
+                    {
+
+                    }
+                    foreach (var error in item.Errors)
+                    {
+                        errorMessages.Add(error.ErrorMessage);
+                    }
+                }
+                //Below is linq with which we made the above code short
+                //if (ModelState.Values.Errors.Count > 0)
+                //{
+
+                //}
+                string errorList = string.Join("\n", ModelState.Values
+                    .SelectMany(errors => errors.Errors)
+                    .Select(errorMessage => errorMessage.ErrorMessage).ToList());
+
+                string fullErrorMessage = string.Join(", ", ModelState.Values
+                    .Where(v => v.Errors.Count > 0) // using count of erors
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage));
+
+
+                return BadRequest(errorList);
+            }
+            return Content($"{person}");
+        }
+    }
+}
---
+++ 06. Model Binding and Validations/08. Custom Validation/ModelValidatonExample/ModelValidatonExample/Models/Person.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidatonExample.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "{0} cannot be empty")]
+        [Display(Name = "person name")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "Name length should be between {2} and {1}")]
+        [RegularExpression("^[A-Za-z .]+$",ErrorMessage ="{0} can only have space and . No other characters are accepted.")]
+        public string? PersonName { get; set; }
+
+        [EmailAddress(ErrorMessage ="Valid email is required !")]
+        [Required(ErrorMessage ="Email is mendatory !")]
+        public string? Email { get; set; }
+
+        [Phone(ErrorMessage ="Valid phone number is required !")]
+        public string? Phone { get; set; }
+
+        [Required(ErrorMessage ="Password is required")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [Compare("Password",ErrorMessage ="{0} doesn't match with {1}.")]
+        [Display(Name ="Confirm Password")]
+        public string? ConfirmedPassword { get; set; }
+
+        [Range(0, 999.99, ErrorMessage = "{0} must be between {1} and {2}")]
+        public string? Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person object - PersonName:{PersonName} Email: {Email} Phone: {Phone} Password: {Password} ConfirmePassword: {ConfirmedPassword} Price: {Price} ";
+        }
+    }
+}
