3d933acb3dd49ca348159f35f2af8603ea3a04f8
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-04-05 21:57:31 +0545

    Add project for 10. IValidation Object

---
+++ 06. Model Binding and Validations/10. IValidationObject/ModelValidatonExample/ModelValidatonExample/CustomValidators/MinimumYearValidatorAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.Serialization;
+
+namespace ModelValidatonExample.CustomValidators
+{
+    public class MinimumYearValidatorAttribute: ValidationAttribute
+    {
+
+        public int _minimumYear { get; set; } = 2000;
+        public string _defaultErrorMessage { get; set; } = "Minimum Birth Year cannot be more than 2000";
+        //parameterless constructor
+        public MinimumYearValidatorAttribute()
+        {
+
+        }
+        public MinimumYearValidatorAttribute(int MimimumYear)
+        {
+            _minimumYear = MimimumYear;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if(value != null)
+            {
+                DateTime date = (DateTime)value;
+                if (date.Year >= _minimumYear)
+                {
+                    return new ValidationResult(string.Format(ErrorMessage ?? _defaultErrorMessage,_minimumYear));
+                }
+                else
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            else
+            {
+                //return new ValidationResult("The value cannot be null");
+                //return base.IsValid(value, validationContext);
+                return null; 
+            }
+        }
+
+    }
+}
