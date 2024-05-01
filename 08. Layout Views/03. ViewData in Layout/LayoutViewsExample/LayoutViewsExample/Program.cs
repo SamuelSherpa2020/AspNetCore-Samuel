29eccbc946be97602a499ee91cbebc69162146ba
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-05-01 13:45:41 +0545

    Add project for ViewData in Layout

---
+++ 08. Layout Views/03. ViewData in Layout/LayoutViewsExample/LayoutViewsExample/Program.cs
@@ -0,0 +1,21 @@
+namespace LayoutViewsExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+             var app = builder.Build();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapControllers();
+
+            //app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
