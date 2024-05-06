97cbda5539719af4aecea9f1b04d2cc6ee8237bd
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-05-06 19:53:32 +0545

    Complete code for return PartialView("_parView",model)

---
+++ 09. Partial Views/04. PartialViewResult/PartialViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewsExample.Models;
+
+namespace PartialViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewData["ListTitle"] = "Cities";
+            ViewData["ListItems"] = new List<string>() {
+        "Paris",
+        "New York",
+        "New Mumbai",
+        "Rome"
+      };
+            return View();
+        }
+
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("/programming-languages")]
+        public IActionResult ProgrammingLanguage()
+        {
+            ListModel model = new ListModel()
+            {
+                TitleName = "Programming Languages",
+                ListItems = new List<string>() {
+                "C#",
+                "C++",
+                "Objective C",
+                "JavaScript",
+                "Python"
+                }
+            };
+
+            return PartialView("_ListPartialView",model);
+        }
+    }
+}
