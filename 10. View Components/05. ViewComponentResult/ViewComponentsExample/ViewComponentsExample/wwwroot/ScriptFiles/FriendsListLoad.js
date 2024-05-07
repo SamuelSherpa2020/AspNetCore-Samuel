a6304500f51d84292590931b1858a16e3844927c
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-05-07 09:25:17 +0545

    Add project and complete for ViewComponent

---
+++ 10. View Components/05. ViewComponentResult/ViewComponentsExample/ViewComponentsExample/wwwroot/ScriptFiles/FriendsListLoad.js
@@ -0,0 +1,14 @@
+﻿document.querySelector("#load-friends-button").addEventListener("click", async function () {
+    try {
+        // Fetch data from the server
+        var response = await fetch("friends-list", { method: "GET" });
+
+        // Extract the response text
+        var responseBody = await response.text();
+
+        // Update the HTML content of the list div
+        document.querySelector("#list").innerHTML = responseBody;
+    } catch (error) {
+        console.error("Error fetching friends list:", error);
+    }
+});
