d2df68ecde0c2b538107cf22b25dd3e0b36a1856
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-05-07 07:50:29 +0545

    Add project and complete it for Strongly typed ViewComponent

---
+++ 10. View Components/03. Strongly Typed View Components/ViewComponentsExample/ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentsExample.Models;
+
+namespace ViewComponentsExample.ViewComponents
+{
+    //[ViewComponent] the attribute is optional
+    public class GridViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            PersonGridModel personGridModel = new PersonGridModel()
+            {
+                GridTitle = "Employee Description",
+                Persons = new List<Person>() {
+                    new Person() { Name = "Samuel", JobTitle = "Sr. Software & DevOps Engineer" },
+                    new Person() { Name = "Kailash", JobTitle = "Sr. React" },
+                    new Person() { Name = "Suman", JobTitle = "Sr. Dotnet" },
+                    new Person() { Name = "Sahil", JobTitle = "Sr. Database Administrator" },
+                    new Person() { Name = "Manjil", JobTitle = "Sr. Quality Assuarance" }
+                }
+            };
+            //ViewData["EmployeesDescription"] = personGridModel;
+            return View("Sample",personGridModel); //invoked a partial view Views/Shared/Components/Grid/Default.cshtml
+        }
+    }
+}
