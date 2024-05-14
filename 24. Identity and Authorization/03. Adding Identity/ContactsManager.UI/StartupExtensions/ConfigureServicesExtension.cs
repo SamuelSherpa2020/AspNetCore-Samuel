a3f261732346c47cb9a17acdc6def398e5fee6d7
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-05-14 23:28:35 +0545

    Complete code for adding Identity in IoC container with Identity Repository (UserStore, RoleStore)

--- 24. Identity and Authorization/03. Adding Identity/ContactsManager.UI/StartupExtensions/ConfigureServicesExtension.cs
+++ 24. Identity and Authorization/03. Adding Identity/ContactsManager.UI/StartupExtensions/ConfigureServicesExtension.cs
@@ -1,5 +1,8 @@
-﻿using CRUDExample.Filters.ActionFilters;
+﻿using ContactsManager.Core.Domain.IdentityEntities;
+using CRUDExample.Filters.ActionFilters;
 using Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Repositories;
 using RepositoryContracts;
@@ -8,55 +11,60 @@ using Services;
 
 namespace CRUDExample
 {
- public static class ConfigureServicesExtension
- {
-  public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
-  {
-   services.AddTransient<ResponseHeaderActionFilter>();
-
-   //it adds controllers and views as services
-   services.AddControllersWithViews(options => {
-    //options.Filters.Add<ResponseHeaderActionFilter>(5);
-
-    var logger = services.BuildServiceProvider().GetRequiredService<ILogger<ResponseHeaderActionFilter>>();
-
-    options.Filters.Add(new ResponseHeaderActionFilter(logger)
+    public static class ConfigureServicesExtension
     {
-     Key = "My-Key-From-Global",
-     Value = "My-Value-From-Global",
-     Order = 2
-    });
-   });
+        public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<ResponseHeaderActionFilter>();
 
-   //add services into IoC container
-   services.AddScoped<ICountriesRepository, CountriesRepository>();
-   services.AddScoped<IPersonsRepository, PersonsRepository>();
+            //it adds controllers and views as services
+            services.AddControllersWithViews(options =>
+            {
+                //options.Filters.Add<ResponseHeaderActionFilter>(5);
 
-   services.AddScoped<ICountriesGetterService, CountriesGetterService>();
-   services.AddScoped<ICountriesAdderService, CountriesAdderService>();
-   services.AddScoped<ICountriesUploaderService, CountriesUploaderService>();
+                var logger = services.BuildServiceProvider().GetRequiredService<ILogger<ResponseHeaderActionFilter>>();
 
-   services.AddScoped<IPersonsGetterService, PersonsGetterServiceWithFewExcelFields>();
-   services.AddScoped<PersonsGetterService, PersonsGetterService>();
+                options.Filters.Add(new ResponseHeaderActionFilter(logger)
+                {
+                    Key = "My-Key-From-Global",
+                    Value = "My-Value-From-Global",
+                    Order = 2
+                });
+            });
 
-   services.AddScoped<IPersonsAdderService, PersonsAdderService>();
-   services.AddScoped<IPersonsDeleterService, PersonsDeleterService>();
-   services.AddScoped<IPersonsUpdaterService, PersonsUpdaterService>();
-   services.AddScoped<IPersonsSorterService, PersonsSorterService>();
+            //add services into IoC container
+            services.AddScoped<ICountriesRepository, CountriesRepository>();
+            services.AddScoped<IPersonsRepository, PersonsRepository>();
 
-   services.AddDbContext<ApplicationDbContext>(options =>
-   {
-    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
-   });
+            services.AddScoped<ICountriesGetterService, CountriesGetterService>();
+            services.AddScoped<ICountriesAdderService, CountriesAdderService>();
+            services.AddScoped<ICountriesUploaderService, CountriesUploaderService>();
 
-   services.AddTransient<PersonsListActionFilter>();
+            services.AddScoped<IPersonsGetterService, PersonsGetterServiceWithFewExcelFields>();
+            services.AddScoped<PersonsGetterService, PersonsGetterService>();
 
-   services.AddHttpLogging(options =>
-   {
-    options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestProperties | Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.ResponsePropertiesAndHeaders;
-   });
+            services.AddScoped<IPersonsAdderService, PersonsAdderService>();
+            services.AddScoped<IPersonsDeleterService, PersonsDeleterService>();
+            services.AddScoped<IPersonsUpdaterService, PersonsUpdaterService>();
+            services.AddScoped<IPersonsSorterService, PersonsSorterService>();
 
-   return services;
-  }
- }
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            //Enable Identity in the project
+            services.AddTransient<PersonsListActionFilter>();
+            services.AddIdentity<ApplicationUser, ApplicationRole>().AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders()
+                .AddUserStore<UserStore<ApplicationUser,ApplicationRole,ApplicationDbContext,Guid>>()
+                .AddRoleStore<RoleStore<ApplicationRole,ApplicationDbContext,Guid>>();
+            services.AddHttpLogging(options =>
+            {
+                options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestProperties | Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.ResponsePropertiesAndHeaders;
+            });
+
+            return services;
+        }
+    }
 }
