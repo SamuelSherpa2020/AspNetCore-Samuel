4f48bce4813da6b2dbe77456438de1bcffcb33bf
samuelsherpa2020 <samuelsherpa46@gmail.com>
2024-05-15 11:27:05 +0545

    Add project for 04. 05. Sign in Manager

--- 24. Identity and Authorization/04. User Manager/ContactsManager.UI/Controllers/AccountController.cs
+++ 24. Identity and Authorization/04. User Manager/ContactsManager.UI/Controllers/AccountController.cs
@@ -1,6 +1,9 @@
-﻿using ContactsManager.Core.DTO;
+﻿using ContactsManager.Core.Domain.IdentityEntities;
+using ContactsManager.Core.DTO;
 using CRUDExample.Controllers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ContactsManager.UI.Controllers
 {
@@ -8,6 +11,11 @@ namespace ContactsManager.UI.Controllers
     [Route("[controller]/[action]")]
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public AccountController(UserManager<ApplicationUser> _applicationUser)
+        {
+            _userManager = _applicationUser;
+        }
 
         [HttpGet]
         public IActionResult Register()
@@ -17,9 +25,40 @@ namespace ContactsManager.UI.Controllers
 
 
         [HttpPost]
-        public IActionResult Register(RegisterDTO registerDTO)
+        public async Task<IActionResult> Register(RegisterDTO registerDTO)
         {
             //to do: Store user register detail in identity database
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Error = ModelState.Values.SelectMany(temp => temp.Errors)
+                    .Select(temp => temp.ErrorMessage)
+                    .ToList();
+                return View(registerDTO);
+
+            }
+            else
+            {
+                ApplicationUser user = new ApplicationUser() { 
+                UserName = registerDTO.Email,
+                PersonName = registerDTO.PersonName,
+                Email = registerDTO.Email,
+                PhoneNumber = registerDTO.Phone,
+                };
+
+                IdentityResult identityResult = await _userManager.CreateAsync(user);
+
+                if (identityResult.Succeeded)
+                {
+                    return RedirectToAction(nameof(PersonsController.Index), "Persons");
+                }
+                else
+                {
+                    foreach (IdentityError erorr in identityResult.Errors)
+                    {
+                        ModelState.AddModelError("Register", erorr.Description);
+                    }
+                }
+            }
             return RedirectToAction(nameof(PersonsController.Index),"Persons");
         }
     }
---
+++ 24. Identity and Authorization/05. SignIn Manager/ContactsManager.UI/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using ContactsManager.Core.Domain.IdentityEntities;
+using ContactsManager.Core.DTO;
+using CRUDExample.Controllers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ContactsManager.UI.Controllers
+{
+
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public AccountController(UserManager<ApplicationUser> _applicationUser)
+        {
+            _userManager = _applicationUser;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterDTO registerDTO)
+        {
+            //to do: Store user register detail in identity database
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Error = ModelState.Values.SelectMany(temp => temp.Errors)
+                    .Select(temp => temp.ErrorMessage)
+                    .ToList();
+                return View(registerDTO);
+
+            }
+            else
+            {
+                ApplicationUser user = new ApplicationUser() { 
+                UserName = registerDTO.Email,
+                PersonName = registerDTO.PersonName,
+                Email = registerDTO.Email,
+                PhoneNumber = registerDTO.Phone,
+                };
+
+                IdentityResult identityResult = await _userManager.CreateAsync(user);
+
+                if (identityResult.Succeeded)
+                {
+                    return RedirectToAction(nameof(PersonsController.Index), "Persons");
+                }
+                else
+                {
+                    foreach (IdentityError erorr in identityResult.Errors)
+                    {
+                        ModelState.AddModelError("Register", erorr.Description);
+                    }
+                }
+            }
+            return RedirectToAction(nameof(PersonsController.Index),"Persons");
+        }
+    }
+}
